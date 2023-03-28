49ff2b807076e5012e7f40749437f614962dc1ee
Connor Prentice <connurp@gmail.com>
2023-03-27 23:22:01 -0500

    now reads correctly need to fix getPage()

--- main.py
+++ main.py
@@ -5,9 +5,16 @@ reader = PyPDF2.PdfFileReader(open("translate.pdf", "rb"))
 speaker = pyttsx3.init()
 
 for page_num in range(reader.numPages):
-    text = reader.getPage(page_num).extractText()
-    clean_text = text.strip().replace("\n", " ")
+    text = reader.getPage(0).extractText()  # need to fix getPage()
+    clean_text = text.strip().replace(" ", "").replace("\n", " ")
     print(clean_text)
+
+rate = speaker.getProperty("rate")
+print(rate)
+speaker.setProperty("rate", 120)
+
+voices = speaker.getProperty("voices")
+speaker.setProperty("voice", voices[1].id)
 
 speaker.save_to_file(clean_text, "translated.mp3")
 speaker.runAndWait()
